f6fbb4828ef34148983dbc5a1f358f942eabaa74
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2023-09-09 21:22:23 +0200

    update scripts

--- customer_landing_to_trusted.py
+++ customer_landing_to_trusted.py
@@ -4,7 +4,8 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-import re
+from awsglue.dynamicframe import DynamicFrame
+from pyspark.sql import functions as SqlFuncs
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -13,37 +14,65 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Script generated for node Customer Landing Zone
-CustomerLandingZone_node1 = glueContext.create_dynamic_frame.from_options(
+# Script generated for node Customer Trusted
+CustomerTrusted_node1693829231273 = glueContext.create_dynamic_frame.from_options(
     format_options={"multiline": False},
     connection_type="s3",
     format="json",
     connection_options={
-        "paths": ["s3://udacity-vgonzenb-lake-house/customer/landing/"],
+        "paths": ["s3://udacity-vgonzenb-lake-house/customer/trusted/"],
         "recurse": True,
     },
-    transformation_ctx="CustomerLandingZone_node1",
+    transformation_ctx="CustomerTrusted_node1693829231273",
 )
 
-# Script generated for node Filter
-Filter_node1693768955914 = Filter.apply(
-    frame=CustomerLandingZone_node1,
-    f=lambda row: (not (row["shareWithResearchAsOfDate"] == 0)),
-    transformation_ctx="Filter_node1693768955914",
+# Script generated for node Accelerometer Trusted
+AccelerometerTrusted_node1 = glueContext.create_dynamic_frame.from_options(
+    format_options={"multiline": False},
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "paths": ["s3://udacity-vgonzenb-lake-house/accelerometer/trusted/"],
+        "recurse": True,
+    },
+    transformation_ctx="AccelerometerTrusted_node1",
 )
 
-# Script generated for node Customer Trusted Zone
-CustomerTrustedZone_node3 = glueContext.getSink(
-    path="s3://udacity-vgonzenb-lake-house/customer/trusted/",
+# Script generated for node Drop Duplicates
+DropDuplicates_node1694275958840 = DynamicFrame.fromDF(
+    AccelerometerTrusted_node1.toDF().dropDuplicates(),
+    glueContext,
+    "DropDuplicates_node1694275958840",
+)
+
+# Script generated for node Customer Privacy Filter
+CustomerPrivacyFilter_node1693829217383 = Join.apply(
+    frame1=CustomerTrusted_node1693829231273,
+    frame2=DropDuplicates_node1694275958840,
+    keys1=["email"],
+    keys2=["user"],
+    transformation_ctx="CustomerPrivacyFilter_node1693829217383",
+)
+
+# Script generated for node Drop Fields
+DropFields_node1693829616513 = DropFields.apply(
+    frame=CustomerPrivacyFilter_node1693829217383,
+    paths=[],
+    transformation_ctx="DropFields_node1693829616513",
+)
+
+# Script generated for node Customer Curated
+CustomerCurated_node3 = glueContext.getSink(
+    path="s3://udacity-vgonzenb-lake-house/customer/curated/",
     connection_type="s3",
     updateBehavior="UPDATE_IN_DATABASE",
     partitionKeys=[],
     enableUpdateCatalog=True,
-    transformation_ctx="CustomerTrustedZone_node3",
+    transformation_ctx="CustomerCurated_node3",
 )
-CustomerTrustedZone_node3.setCatalogInfo(
-    catalogDatabase="lakehouse", catalogTableName="customer_trusted"
+CustomerCurated_node3.setCatalogInfo(
+    catalogDatabase="lakehouse", catalogTableName="customer_curated"
 )
-CustomerTrustedZone_node3.setFormat("json")
-CustomerTrustedZone_node3.writeFrame(Filter_node1693768955914)
+CustomerCurated_node3.setFormat("json")
+CustomerCurated_node3.writeFrame(DropFields_node1693829616513)
 job.commit()
--- customer_trusted_to_curated.py
+++ customer_trusted_to_curated.py
@@ -4,6 +4,8 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
+from pyspark.sql import functions as SqlFuncs
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -24,24 +26,31 @@ CustomerTrusted_node1693829231273 = glueContext.create_dynamic_frame.from_option
     transformation_ctx="CustomerTrusted_node1693829231273",
 )
 
-# Script generated for node Accelerometer Landing
-AccelerometerLanding_node1 = glueContext.create_dynamic_frame.from_options(
+# Script generated for node Accelerometer Trusted
+AccelerometerTrusted_node1 = glueContext.create_dynamic_frame.from_options(
     format_options={"multiline": False},
     connection_type="s3",
     format="json",
     connection_options={
-        "paths": ["s3://udacity-vgonzenb-lake-house/accelerometer/landing"],
+        "paths": ["s3://udacity-vgonzenb-lake-house/accelerometer/trusted/"],
         "recurse": True,
     },
-    transformation_ctx="AccelerometerLanding_node1",
+    transformation_ctx="AccelerometerTrusted_node1",
+)
+
+# Script generated for node Drop Duplicates
+DropDuplicates_node1694275958840 = DynamicFrame.fromDF(
+    AccelerometerTrusted_node1.toDF().dropDuplicates(["user"]),
+    glueContext,
+    "DropDuplicates_node1694275958840",
 )
 
 # Script generated for node Customer Privacy Filter
 CustomerPrivacyFilter_node1693829217383 = Join.apply(
-    frame1=AccelerometerLanding_node1,
-    frame2=CustomerTrusted_node1693829231273,
-    keys1=["user"],
-    keys2=["email"],
+    frame1=CustomerTrusted_node1693829231273,
+    frame2=DropDuplicates_node1694275958840,
+    keys1=["email"],
+    keys2=["user"],
     transformation_ctx="CustomerPrivacyFilter_node1693829217383",
 )
 
